085f07a089a6dd3254e0ad4830b90efbf4e50060
kairosgomide <123810831+kairosgomide@users.noreply.github.com>
2023-12-28 19:12:10 -0300

    Add files via upload

---
+++ secao_5/Lista/Exercicio10_1.py
@@ -0,0 +1,6 @@
+km = int(input(f'Escreva a distância total percorrida, em quilômetros: '))
+combustivel = int(input(f'Escreva a quantidade total de combustivel gasto em litros : '))
+
+media_de_consumo = km / combustivel
+
+print(f'A média de consumo deste automovel é {media_de_consumo}km/l')
