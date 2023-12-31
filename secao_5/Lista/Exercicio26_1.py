ce725cc4e83f143e2d9718a9a9d13437b06721f0
kairosgomide <123810831+kairosgomide@users.noreply.github.com>
2023-12-31 16:02:12 -0300

    Delete secao_5/Lista/Exercicio26_1.py

--- secao_5/Lista/Exercicio26_1.py
+++
@@ -1,9 +0,0 @@
-c = int(input('Digite um valor de 0 a 10: '))
-d = int(input('Digite um valor de 0 a 10: '))
-u = int(input('Digite um valor de 0 a 10: '))
-
-centena = c * (10 ** 2)
-dezena = d * (10 ** 1)
-unidade = u * (10 ** 0)
-
-
