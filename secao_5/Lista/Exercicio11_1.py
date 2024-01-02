ee5bfb69e23dcd382886d5f1ba652c2386df905b
kairosgomide <123810831+kairosgomide@users.noreply.github.com>
2024-01-02 09:05:37 -0300

    Add files via upload

--- secao_5/Lista/Exercicio11_1.py
+++ secao_5/Lista/Exercicio11_1.py
@@ -1,5 +1,10 @@
+import math
+
 raio_circunferencia = int(input(f'Dê uma valor ao raio de uma circunferência: '))
 
-area = (raio_circunferencia * raio_circunferencia) * 3.14159
+# area = (raio_circunferencia * raio_circunferencia) * 3.14159
 
+area = (raio_circunferencia ** 2) * math.pi
+
+print(f'Valor de pi {math.pi}')
 print(f'A área dessa circunferência é {area} metros')
