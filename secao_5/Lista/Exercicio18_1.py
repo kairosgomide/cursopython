270a9c44337300693a7f69c4e367efff4ef25ac4
kairosgomide <123810831+kairosgomide@users.noreply.github.com>
2024-01-02 09:14:36 -0300

    Add files via upload

--- secao_5/Lista/Exercicio18_1.py
+++ secao_5/Lista/Exercicio18_1.py
@@ -1,6 +1,6 @@
-real = int(input('Digite um valor em real: '))
+real = float(input('Digite um valor em real: '))
 
-dolar = real // 3.81
-euro = real // 4.17
+dolar = real / 3.81
+euro = real / 4.17
 
-print(f'convertendo do real para dólar temos: US${dolar}, e para euro temos: €{euro}')
+print(f'convertendo do real para dólar temos: US$ {dolar}, e para euro temos: € {euro}')
